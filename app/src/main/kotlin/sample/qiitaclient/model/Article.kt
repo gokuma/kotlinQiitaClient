dc12b3bea531dc150fbc91803b8134a09effe92e
gokuma <go55kmg@gmail.com>
2018-04-27 22:53:29 +0900

    add models

---
+++ app/src/main/kotlin/sample/qiitaclient/model/Article.kt
@@ -0,0 +1,7 @@
+package sample.qiitaclient.model
+
+
+data class Article(val id: String,
+                   val title: String,
+                   val url: String,
+                   val user: User)
---
+++ app/src/main/kotlin/sample/qiitaclient/model/User.kt
@@ -0,0 +1,6 @@
+package sample.qiitaclient.model
+
+
+data class User(val id: String,
+                val name: String,
+                val profileImageUrl: String)
