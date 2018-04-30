54af009ef93d737920bb160f2f8a64dddc1b77f6
gokuma <go55kmg@gmail.com>
2018-04-30 13:04:27 +0900

    viewを追加

--- app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
@@ -1,12 +1,30 @@
 package sample.qiitaclient
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ListView
+import sample.qiitaclient.model.Article
+import sample.qiitaclient.model.User
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listAdapter = ArticleListAdapter(applicationContext)
+        listAdapter.articles = listOf(dummyArticle("Kotlin入門", "太郎"),
+                dummyArticle("Java入門", "二郎"))
+
+        val listView: ListView = findViewById(R.id.list_view) as ListView
+        listView.adapter = listAdapter
+
     }
+
+    private fun dummyArticle(title: String, userName: String): Article =
+            Article(id = "",
+                    title = title,
+                    url = "https://kotlinlang.org/",
+                    user = User(id = "", name = userName, profileImageUrl = ""))
+
 }
---
+++ app/src/main/kotlin/sample/qiitaclient/extentions.kt
@@ -0,0 +1,9 @@
+package sample.qiitaclient
+
+
+import android.support.annotation.IdRes
+import android.view.View
+
+fun <T : View> View.bindView(@IdRes id: Int): Lazy<T> = lazy {
+    findViewById(id) as T
+}
